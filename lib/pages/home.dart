e11d6b272f6e1ee57fd2be1790542b54a462edc6
galdino01 <lucaspolucena@hotmail.com>
2022-08-18 14:48:12 -0300

    Adding carousel to view stock indexes

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,11 @@
-import 'package:currency_converter/main.dart';
 import 'package:flutter/material.dart';
 
+import 'package:carousel_slider/carousel_slider.dart';
+
+import 'package:currency_converter/main.dart';
 import 'package:currency_converter/widgets/custom_text_field.dart';
+
+import 'dart:convert';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -15,6 +19,8 @@ class _HomeState extends State<Home> {
   final _dollarController = TextEditingController();
   final _euroController = TextEditingController();
 
+  late Map<String, dynamic> carouselItems;
+  late List<String> keys;
   late double dollar;
   late double euro;
 
@@ -98,6 +104,8 @@ class _HomeState extends State<Home> {
                   ),
                 );
               } else {
+                carouselItems = snapshot.data!['results']['stocks'];
+                keys = carouselItems.keys.toList();
                 dollar = snapshot.data!['results']['currencies']['USD']['buy'];
                 euro = snapshot.data!['results']['currencies']['EUR']['buy'];
 
@@ -113,7 +121,8 @@ class _HomeState extends State<Home> {
                       ),
                       CustomTextField(
                         type: const TextInputType.numberWithOptions(
-                            decimal: true),
+                          decimal: true,
+                        ),
                         controller: _realController,
                         label: 'Reais',
                         prefix: 'R\$ ',
@@ -122,7 +131,8 @@ class _HomeState extends State<Home> {
                       const Divider(color: Colors.white),
                       CustomTextField(
                         type: const TextInputType.numberWithOptions(
-                            decimal: true),
+                          decimal: true,
+                        ),
                         controller: _dollarController,
                         label: 'Dolares',
                         prefix: 'US\$ ',
@@ -131,11 +141,139 @@ class _HomeState extends State<Home> {
                       const Divider(color: Colors.white),
                       CustomTextField(
                         type: const TextInputType.numberWithOptions(
-                            decimal: true),
+                          decimal: true,
+                        ),
                         controller: _euroController,
                         label: 'Euros',
                         prefix: '€ ',
                         onChanged: _euroChanged,
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(top: 60),
+                        child: CarouselSlider(
+                          options: CarouselOptions(
+                            height: 100,
+                            viewportFraction: 1,
+                            initialPage: 0,
+                            enlargeCenterPage: false,
+                            enableInfiniteScroll: true,
+                            autoPlay: true,
+                          ),
+                          items: keys.map((key) {
+                            return Builder(builder: (context) {
+                              return Container(
+                                margin: const EdgeInsets.fromLTRB(5, 0, 5, 0),
+                                padding: const EdgeInsets.all(8),
+                                decoration: BoxDecoration(
+                                  color: Colors.black,
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                                child: Column(
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.stretch,
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: [
+                                    Center(
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          Text(
+                                            key,
+                                            style: const TextStyle(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 20,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    const Expanded(
+                                      child: SizedBox(
+                                        width: 8,
+                                      ),
+                                    ),
+                                    Center(
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          Container(
+                                            padding: const EdgeInsets.all(8),
+                                            decoration: BoxDecoration(
+                                              color: Colors.blue,
+                                              borderRadius:
+                                                  BorderRadius.circular(5),
+                                            ),
+                                            child: Text(
+                                              carouselItems[key]['points']
+                                                  .toString(),
+                                              style: const TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 10,
+                                              ),
+                                            ),
+                                          ),
+                                          const SizedBox(width: 8),
+                                          Container(
+                                            padding: const EdgeInsets.all(8),
+                                            decoration: BoxDecoration(
+                                              color: Colors.blue,
+                                              borderRadius:
+                                                  BorderRadius.circular(5),
+                                            ),
+                                            child: Text(
+                                              carouselItems[key]['variation']
+                                                  .toString(),
+                                              style: const TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 10,
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    const Expanded(
+                                      child: SizedBox(
+                                        width: 8,
+                                      ),
+                                    ),
+                                    Center(
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Text(
+                                            carouselItems[key]['name'],
+                                            style: const TextStyle(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 10,
+                                            ),
+                                          ),
+                                          Text(
+                                            carouselItems[key]['location'],
+                                            style: const TextStyle(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 10,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            });
+                          }).toList(),
+                        ),
                       ),
                     ],
                   ),
