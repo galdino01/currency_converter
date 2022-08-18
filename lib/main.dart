db06ae3a95663bbb577102b0ec867c725dc1fa7f
galdino01 <lucaspolucena@hotmail.com>
2022-08-18 10:05:34 -0300

    Adding a connection to the currency conversion api and implementing the getData() function

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() {
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+final _apiKey = dotenv.env['HGBRASIL_API_KEY']!;
+final _uri = "https://api.hgbrasil.com/finance?format=json&key=$_apiKey";
+
+void main() async {
   runApp(MaterialApp(
     home: Container(),
   ));
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(Uri.parse(_uri));
+  return json.decode(response.body);
 }
