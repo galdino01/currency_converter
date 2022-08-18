12d36981fcd4228475b861797e18cc37feeb0515
galdino01 <lucaspolucena@hotmail.com>
2022-08-18 10:53:50 -0300

    Adding text fields on the home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,6 +9,9 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  double dollar = 0.0;
+  double euro = 0.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,8 +50,56 @@ class _HomeState extends State<Home> {
                   ),
                 );
               } else {
-                return Container(
-                  color: Colors.red,
+                dollar = snapshot.data!['results']['currencies']['USD']['buy'];
+                euro = snapshot.data!['results']['currencies']['EUR']['buy'];
+
+                return SingleChildScrollView(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: const [
+                      Icon(
+                        Icons.monetization_on,
+                        size: 150,
+                        color: Colors.amber,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Reais',
+                          labelStyle: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                          ),
+                          border: OutlineInputBorder(),
+                          prefixText: 'R\$ ',
+                        ),
+                      ),
+                      Divider(color: Colors.white),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Dolares',
+                          labelStyle: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                          ),
+                          border: OutlineInputBorder(),
+                          prefixText: 'US\$ ',
+                        ),
+                      ),
+                      Divider(color: Colors.white),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Euros',
+                          labelStyle: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                          ),
+                          border: OutlineInputBorder(),
+                          prefixText: '€ ',
+                        ),
+                      ),
+                    ],
+                  ),
                 );
               }
           }
